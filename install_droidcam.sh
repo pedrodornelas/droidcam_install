90294a13af1c1bc2913299e3a1f2795ce08bf6ea
Pedro Dornelas <phdornelas.almeida@gmail.com>
2020-12-04 17:55:58 -0300

    Atalho menu

--- install_droidcam.sh
+++ install_droidcam.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "--------Desinstalando Droidcam--------"
+echo "--------Desinstalando Droidcam--------\n"
 /opt/droidcam-uninstall
-echo "--------Desinstalação Concluída--------"
+echo "--------Desinstalação Concluída--------\n"
 
-echo "--------Instalando Droidcam--------"
+echo "--------Instalando Droidcam--------\n"
+
 cd /tmp/
 wget https://files.dev47apps.net/linux/droidcam_latest.zip
 unzip droidcam_latest.zip -d droidcam
@@ -12,4 +13,9 @@ cd droidcam && ./install-client
 apt install linux-headers-`uname -r` gcc make
 ./install-video
 apt-get install adb
-echo "--------Instalação Concluída--------"
+
+#Criando atalho no menu de aplicativos
+cp droidcam.desktop ~/.local/share/applications/droidcam.desktop
+cp droidcam.png ~/.local/share/icons/droidcam.png
+
+echo "--------Instalação Concluída--------\n"
